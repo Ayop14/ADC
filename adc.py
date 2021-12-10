4d9ee1f4d673e7c09e95ad21c2a6334f4cc6a9cd
blank14 <57792677+blank14@users.noreply.github.com>
2021-12-10 21:09:30 +0100

    Update adc.py

--- adc.py
+++ adc.py
@@ -1,4 +1,26 @@
-def main():
+import numpy as np
+
+class Node:
+  def __init__(self,clase,D,r,izq,der):
+    self.clase = clase
+    self.D = D
+    self.r = r
+    self.izq = izq
+    self.der = der
+
+def adc(data, e):
+  #encuentra la mejor division 
+  j,r,z =mejor_division(data)
+  #si el decremento de impureza es muy pequeño
+  if z > e:
+    c = clase_dominante(data)
+    return Node(c,None,None,None,None)
+  else:
+    yl = yr = []
+    for 
+  
+  
+  
   
 if __name__ == "__main__":
   main()
