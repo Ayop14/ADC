381774eec49771cd978a9d1188103ff9a84e0f6c
blank14 <betollopis15@gmail.com>
2021-12-21 08:23:21 +0100

    Implementado decremento de impureza

--- adc.py
+++ adc.py
@@ -38,7 +38,7 @@ def mergeSort(arr,indexes):
                 j += 1
             k += 1
   
-        # añadir todos los elementos restantes
+        # anyadir todos los elementos restantes
         while i < len(L):
             arr[k] = L[i]
             indexes[k] = Li[i]
@@ -51,54 +51,75 @@ def mergeSort(arr,indexes):
             j += 1
             k += 1
 
-class adc:
-  def __init__(self,data,index):
-    #para los metodos de mergesort, es necesario tambien pasarles la dimension
-    #en la que estan trabajando actualmente para evitar hacer copias
-    #inicio/final/dimension actual/dimensiones totales
-    #breakpoint()
-    self.data = data
-    np.genfromtxt(file,dtype=float,delimiter=",")
-    self.indexes = indexes
-    print(self.data)
-    print(self.indexes)
+def adc(data, indexes,e):
+  #encuentra la mejor division 
+  j,r,z = mejor_division(data,indexes)
+  #si el decremento de impureza es lo suficientemente pequenyo
+  if z < e:
+    #entonces buscamos la clase dominante
+    c = clase_dominante(data)
+    #lo declaramos como un nodo lo suficientemente puro
+    return Node(c,None,None,None,None)
+  '''
+  Con la indexacion de listas usando listas, igual no es necesaro filtrar
+  else:
+    yl = yr = np.array([])
+    for muestra in data:
+      if muestra[j] <= r:
+        yl = yl.concatenate(muestra)
+      else:
+        yr = yr.concatenate(muestra)
+  '''
+  tl = adc
+  tr = adc
+  return Node(0,j,r,tl,tr)
 
-  def solve(data, e):
-    #encuentra la mejor division 
-    j,r,z = mejor_division(data)
-    #si el decremento de impureza es lo suficientemente pequenyo
-    if z > e:
-      #entonces buscamos la clase dominante
-      c = clase_dominante(data)
-      #lo declaramos como un nodo lo suficientemente puro
-      return Node(c,None,None,None,None)
-    else:
-      yl = yr = np.array([])
-      for muestra in data:
-        if muestra[j] <= r:
-          yl = yl.concatenate(muestra)
-        else:
-          yr = yr.concatenate(muestra)
-    tl = self.solve(yl,e)
-    tr = self.solve(yr,e)
-    return Node(0,j,r,tl,tr)
+def mejor_division(data,indexes):
+  j = r = z = None
+  muestras,D = data.shape
+  #hallamos el indice intermedio
+  indice = len(indexes[0])//2
+  #recorremos cada una de las dimensiones
+  for d in range(D-1):
+    #calculamos el umbral como la media del intermedio y el anterior
+    #len([1,2]) = 2 ->//2 = 1 -> si lo hicieramos con el siguiente daria error,
+    #y si el array es de longitud par entonces no estariamos calculando el intermedio
+    #len([1,2,3,4]) = 4//2=2 arr[2] -> 3a posicion-> pos-1 operamos con 2 y 3 
+    raux = (indexes[d,indice] + indexes[d,indice-1])/2
+    jaux = d
+    zaux = decremento_impureza(data,indexes,jaux)
+    if(j is None or zaux > z):
+      j,r,z = jaux,raux,zaux
+  return (j,r,z)
 
-  def mejor_division(self,data):
-    j = r = None
-    muestras,D = data.shape
-    #me voy a disfrazar en halloween de esta linea
-    split = int(self.data[0,self.index[0,len(self.index[0])//2]])
-    for d in range(D-1):
-      part_inferior = 1
-      part_superior =  1
+#evaluamos la calidad de la particion usando la entropia como medida
+def decremento_impureza(data,indexes,j):
+  indice = len(indexes[j])//2
+  entr = entropia(data)
+  #esto de poder indexar listas CON LISTAS me parece increible
+  entrL = entropia(data[indexes[j,:indice]])
+  entrR = entropia(data[[indexes[j,indice:]])
+  return (entr - entrL*len(indexes[j,:indice]) - enterR*len([indexes[j,indice:]))
 
-  def clase_dominante(data):
-    c = Counter(data[:,-1])
-    res,_ = c.most_common(1)
-    return res
+def entropia(data):
+  #para cada una de las clases
+  C = np.unique(data[:,-1],return_counts = True)
+  N,_ = data.shape
+  suma = 0
+  #calculamos el sumatorio de la entropia
+  #c = clase, Nc = n elementos de esa clase, N = n elementos total
+  for c,Nc in C:
+    suma += (Nc/N * np.log2(Nc/N))
+  #al devolverlo, lo tenemos que volver positivo:
+  return (-suma)
+
+
+def clase_dominante(data):
+  c = Counter(data[:,-1])
+  res,_ = c.most_common(1)
+  return res
 
 if __name__ == "__main__":
-  breakpoint()
   data = np.genfromtxt(sys.argv[1],dtype=float,delimiter=",")
   #obtenemos la forma de las muestras
   muestras,D = data.shape
@@ -106,9 +127,8 @@ if __name__ == "__main__":
   indexes = np.tile(np.arange(muestras),(D-1,1))
   for d in range(D-1):
     mergeSort(data[:,d],indexes[d])
-  print(indexes)
   #e = input('Introduce decremento de impureza minimo: ')
-  #sol = adc(np.genfromtxt(sys.argv[1],dtype=float,delimiter=","),indexes)
+  sol = adc(np.genfromtxt(sys.argv[1],dtype=float,delimiter=","),indexes,e)
   
 
 
